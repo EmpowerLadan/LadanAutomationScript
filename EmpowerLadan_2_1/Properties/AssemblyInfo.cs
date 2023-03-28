cd760bc755635292eca773e3db1aacf5e030c285
LadanRazeghi <ladan.razeghi@sky.uk>
2023-03-28 14:47:15 +0100

    Add project files.

---
+++ EmpowerLadan_2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerLadan_2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerLadan_2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("CD6423FD-6D9C-4499-BF21-6456769266AE")]
+[assembly: AssemblyVersion("1.0.0.0")]
